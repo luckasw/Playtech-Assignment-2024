144228269bc955eb9045f9b4cfe3a03e021687f4
luckasw <Albert.Wihler@gmail.com>
2024-04-04 17:28:57 +0300

    Account object added

---
+++ src/main/java/Account.java
@@ -0,0 +1,37 @@
+public class Account {
+    private final String accountNumber;
+    private final String accuntHolderId;
+    private final String accountCountry;
+    private boolean isValid;
+
+    public Account(String accountNumber, String accuntHolderId, String accountCountry, boolean isValid) {
+        this.accountNumber = accountNumber;
+        this.accuntHolderId = accuntHolderId;
+        this.accountCountry = accountCountry;
+        this.isValid = isValid;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccuntHolderId() {
+        return accuntHolderId;
+    }
+
+    public String getAccountCountry() {
+        return accountCountry;
+    }
+
+    public boolean isValid() {
+        return isValid;
+    }
+
+    public void setAccountValid() {
+        isValid = true;
+    }
+
+    public void setAccountInvalid() {
+        isValid = false;
+    }
+}
