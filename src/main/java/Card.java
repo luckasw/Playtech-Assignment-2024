f0917e40c58e5ceab6fff9192e01bf99d8ea86ea
luckasw <Albert.Wihler@gmail.com>
2024-04-04 17:26:21 +0300

    Card object added

---
+++ src/main/java/Card.java
@@ -0,0 +1,36 @@
+public class Card {
+    private final String cardNumber;
+    private final String cardHolderId;
+    private final String cardCountry;
+    private boolean isValid;
+
+    public Card(String cardNumber, String cardHolderId, String cardCountry, boolean isValid) {
+        this.cardNumber = cardNumber;
+        this.cardHolderId = cardHolderId;
+        this.cardCountry = cardCountry;
+        this.isValid = isValid;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public String getCardHolderId() {
+        return cardHolderId;
+    }
+
+    public String getCardCountry() {
+        return cardCountry;
+    }
+
+    public boolean isValid() {
+        return isValid;
+    }
+
+    public void setCardValid() {
+        isValid = true;
+    }
+    public void setCardInvalid() {
+        isValid = false;
+    }
+}
